ff97975f243d4eca6d3a73463bd097e20f453309
Wooster5 <serkin.ka05@gmail.com>
2023-12-12 15:04:35 +0300

    Add 12.12.2024_17.3 change 12.12.2023_17.1

--- 12.12.2023_17.1.py
+++ 12.12.2023_17.1.py
@@ -1,4 +1,14 @@
-
-def stepen(x:float, n:int):
-    res=0
-    res=
+try:
+    def stepen(x, n):
+        x1=1
+        for i in range(n):
+            x1=x1*x
+        return x1
+    def F(k, a):
+        res=stepen(2.7, k)+stepen(a+1, -5)
+        return(res)
+    k=int(input("Введите К: "))
+    a = float(input("Введите a: "))
+    print(F(k, a))
+except:
+    print("Ошибка!")
