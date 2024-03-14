72307e4d08414f87e8e3229d841bee194256e4ad
Wooster5 <serkin.ka05@gmail.com>
2024-03-14 11:27:49 +0300

    update

--- lab3_17.2/lab3_17.2.cpp
+++ lab3_17.2/lab3_17.2.cpp
@@ -1,21 +1,29 @@
 #include <iostream>
 #include <string>
 #include <cctype>
+#include <locale>
+#include <Windows.h>
 using namespace std;
 
 int main()
 {
-	int len_str;
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
 	string input_str;
 	setlocale(LC_ALL, "Russian");
 	cout << "Введите строку: ";
 	getline(cin, input_str);
 	cout << "Вы ввели: " << input_str << endl;
 
-	for (char& ch : input_str) {
-		ch = toupper(ch);
+	for (char& ch : input_str) 
+	{
+		if (isalpha(ch) && isupper(ch))
+		{
+			ch = tolower(ch);
+		}
+		
 	}
 
-	cout << "Количество цифр: "<<input_str;
+	cout << "Новая строка: "<<input_str;
 	return 0;
 }
