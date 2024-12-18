867109fa8c0be6f4d2f998ac371026483d8039dc
Wooster5 <serkin.ka05@gmail.com>
2024-12-18 15:18:01 +0300

    add bin

--- last_lab/main.cpp
+++ last_lab/main.cpp
@@ -4,7 +4,9 @@
 int main() {
     std::vector<int> arr = { 5, 2, 8, 1, 9 };
     setlocale(LC_ALL, "rus");
+ 
 
+    int valuex = 9;
 
     sortArray(arr);
     std::cout << "ќтсортированный массив: ";
@@ -26,6 +28,15 @@ int main() {
 
     double avg = calculateAverage(arr);
     std::cout << "—реднее значение: " << avg << std::endl;
+
+    int index1 = binarySearch(arr, valuex);
+
+    if (index1 != -1) {
+        std::cout << "Ёлемент " << valuex << " найден на индексе " << index1 << "." << std::endl;
+    }
+    else {
+        std::cout << "Ёлемент " << valuex << " не найден." << std::endl;
+    }
 
     return 0;
 }
